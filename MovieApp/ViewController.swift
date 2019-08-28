4d1efd4cc6d4e60710261f1c1a1ee8efacecabb3
Aldo Martinez <aldo.martinez@globant.com>
2019-08-28 02:43:31 -0500

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Aldo Antonio Martinez Avalos on 8/28/19.
+//  Copyright © 2019 aavalosmt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
